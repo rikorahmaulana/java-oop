797f4dca118b6bb76f19022c9796f5aa7b796bf3
rik <riko.rmaulana2812@gmail.com>
2023-10-28 14:24:41 +0700

    update

--- src/Person.java
+++ src/Person.java
@@ -1,17 +1,16 @@
 class Person {
 
-	/*
-	- fields/properties/attributes adalah data yang bisa kita sisipkan di dalam object
-	- namum sebelum memasukan data di fields kita harus mendeklarasikan data apa saja yang dimiliki object tersebut di dalam deklarasi classnya
-	- membuat fields sama seperti membuat variable, namun di tempatkan di block class
-	*/ 
-
-	/*
-	- fileds final tidak bisa di manipulasi
-	- untuk mengakses field, butuh kata kunci .(titik) setelah nama object dan diikuti nama fields nya
-	*/
 	String name;
 	String address;
 	final String country = "Indonesia"; // final adalah field yg tidak bisa di ubah
+
+	Person(String paramName, String paramAddress) {
+		name = paramName;
+		address = paramAddress;
+	}
+
+	void sayHello (String paramName) {
+		System.out.println("Hello" + paramName + ", My Name is " + name);
+	}
 
 }
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,22 +1,17 @@
 public class PersonApp {
 	public static void main(String[] args){
-		// object adalah hasil dari instanisasi dari sebuah class
-		// untuk membuat object kita bisa menggunakan kata kunci new, dan diikuti dengan nama class dan kurung ()
 
-		var person1 = new Person();
-
-		// manipulasi fields
-		person1.name = "riko";
-		person1.address = "cikarang";
-		// person1.country = "jakarta"; ini akan eror karena menggunakan final di fields nya
-
+		var person1 = new Person("riko", "cikarang");
 		System.out.println(person1.name);
 		System.out.println(person1.address);
 		System.out.println(person1.country);
 
-		Person person2 = new Person();
+		person1.sayHello(" Budi");
+		Person person2 = new Person("Budi", "Jakarta");
+		
 		Person person3;
-		person3 = new Person();
+		person3 = new Person("Joko", "Cirebon");
+		person3.sayHello(" Budi");
 
 	}
 }
